da252b3814fc16f2c55a6ecddf10793344053bab
alex barlow <alex.barlow@rocketmail.com>
2023-01-29 13:14:16 -0500

    added WebpackPwaManifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,7 +29,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Textm Editor for PWA Assignment',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96,128,192,256,384,512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
